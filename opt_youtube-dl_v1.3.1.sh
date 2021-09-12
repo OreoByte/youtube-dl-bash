13c2a4065cd9f2762242ded2bde710c502dfc002
IO-Shield <isaacoren42@gmail.com>
2021-09-12 08:58:41 -0700

    filter all '#' song title comments from url.lst file

--- opt_youtube-dl_v1.3.1.sh
+++ opt_youtube-dl_v1.3.1.sh
@@ -22,16 +22,17 @@ do
 esac
 done
 fi
-
 if [[ -z "$link" ]]; then
 	if [[ -z "$file" ]]; then
 		printf "Error No Link 0r File Provided"
 	elif [ "$f_type" == 'mp4' ] || [ "$f_type" == 'video' ] || [ "$f_type" == 'v' ]; then
-		while IFS= read -r line; do youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' $line; done < "$file"
+		title_file=$(grep "http" $file)
+		while IFS= read -r line; do youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' $line; done < <(printf '%s\n' "$title_file")
 		# pulls the best quilty it could be for that video. Saved as a .mp4 file
 		exit 1
 	elif [ "$f_type" == 'mp3' ] || [ "$f_type" == 'audio' ] || [ "$f_type" == 'a' ]; then
-		while IFS= read -r line; do youtube-dl -x -f bestaudio --audio-quality 0 --audio-format mp3 $line; done < "$file"
+		title_file=$(grep "http" $file)
+		while IFS= read -r line; do youtube-dl -x -f bestaudio --audio-quality 0 --audio-format mp3 $line; done < <(printf '%s\n' "$title_file")
 		# pulls the best quality audio for "X" video. Saved as a .mp3 file
 		exit 1
         else
